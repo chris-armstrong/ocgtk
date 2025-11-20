2af108574d22e33f8020474b6a77c55f347d5c53
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2025-11-20 15:13:17 +1100

    fix: gobject crash

--- lablgtk4/src/wrappers.h
+++ lablgtk4/src/wrappers.h
@@ -181,7 +181,7 @@ void* ext_of_val(value val);
 #define Val_GObject(obj) (val_of_ext(obj))
 
 /* GClosure - custom block with finalizer (defined in ml_gobject.c) */
-#define GClosure_val(val) *((GClosure**)Data_custom_val(val))
+#define GClosure_val(val) (*((GClosure**)Data_custom_val(val)))
 
 /* GType */
 #define GType_val(val) ((GType)Long_val(val))
