4d4ae5bc32df73e6bc2d30cae30183bf62567380
Chris Armstrong <chris-armstrong@users.noreply.github.com>
2026-01-30 19:27:34 +1100

    refactor: extract constructor and hierarchy helper functions
    Guideline: Max 2 levels nesting, single-purpose functions
    Changes:
    - Extracted build_constructor_signature, format_constructor_external
    - Extracted should_generate_accessor, build_accessor_base_type, format_accessor_declaration
    - Extracted build_parent_chain_variants, build_hierarchy_variants
    Analysis: Reduces nesting from 4 levels to 2, improves testability by allowing unit testing of individual components

--- ocgtk/src/tools/gir_gen/generate/ml_interface.ml
+++ ocgtk/src/tools/gir_gen/generate/ml_interface.ml
@@ -49,55 +49,51 @@ let qualify_module_reference ~ctx module_type =
 let generate_signal_bindings ~output_mode:_ ~module_name:_ ~has_widget_parent:_ _signals =
   ""  (* Signals are only generated in high-level g*.ml wrappers *)
 
+(** Build parent chain variants up to hierarchy root *)
+let build_parent_chain_variants ~normalized_class ~parent_chain ~hier_info =
+  if String.lowercase_ascii normalized_class = String.lowercase_ascii hier_info.gir_root then []
+  else
+    List.fold_left parent_chain ~init:[] ~f:(fun acc parent ->
+      let tag = "`" ^ (Utils.to_snake_case parent |> Utils.sanitize_identifier) in
+      acc @ [tag])
+
+(** Build hierarchy variants for a monomorphic class *)
+let build_hierarchy_variants ~normalized_class ~parent_chain =
+  let self_variant = "`" ^ (Utils.to_snake_case normalized_class |> Utils.sanitize_identifier) in
+  let parent_variants = List.map parent_chain ~f:(fun p -> "`" ^ (Utils.to_snake_case p |> Utils.sanitize_identifier)) in
+  let all_variants = self_variant :: parent_variants in
+  String.concat ~sep:" | " all_variants
+
 let detect_class_hierarchy_names ~ctx ~class_name ~parent_chain ?record_base_type ?(is_record = false) () =
   let normalized_class = Utils.normalize_class_name class_name in
-      let parent_chain =
+  let parent_chain =
     if is_record then []
-    else
-      (* parent_chain is ordered immediate parent -> root *)
+    else (* parent_chain is ordered immediate parent -> root *)
       List.map ~f:Utils.normalize_class_name parent_chain
   in
   if is_record then
-      ("Record", Option.value record_base_type ~default:"Obj.t")
-    else
-      (* Use hierarchy detection to determine type *)
-      match Hierarchy_detection.get_hierarchy_info ctx class_name with
-      | Some hier_info when hier_info.hierarchy <> MonomorphicType ->
-          (* This class is in a known hierarchy *)
-          let self_variant = "`" ^ (Utils.to_snake_case normalized_class |> Utils.sanitize_identifier) in
-          let parent_variants =
-            if String.lowercase_ascii normalized_class = String.lowercase_ascii hier_info.gir_root then
-              (* This is the root of the hierarchy *)
-              []
-            else
-              (* Build parent chain variants up to hierarchy root *)
-              List.fold_left parent_chain ~init:[] ~f:(fun acc parent ->
-                let tag = "`" ^ (Utils.to_snake_case parent |> Utils.sanitize_identifier) in
-                (* Stop at hierarchy root *)
-                if String.lowercase_ascii parent = String.lowercase_ascii hier_info.gir_root then
-                  acc @ [tag]
-                else
-                  acc @ [tag]
-              )
-          in
-          let variants = String.concat ~sep:" | " (self_variant :: parent_variants) in
-          let type_name = match hier_info.hierarchy with
-            | WidgetHierarchy -> "Widget"
-            | EventControllerHierarchy -> "Event controller"
-            | CellRendererHierarchy -> "Cell renderer"
-            | LayoutManagerHierarchy -> "Layout manager"
-            | ExpressionHierarchy -> "Expression"
-            | MonomorphicType -> class_name
-          in
-          (type_name, sprintf "[%s] Gobject.obj" variants)
-      | _ ->
-          (* Monomorphic type or unknown - still use polymorphic variant since all GTK classes inherit from GObject *)
-          let self_variant = "`" ^ (Utils.to_snake_case normalized_class |> Utils.sanitize_identifier) in
-          (* Add parent chain variants *)
-          let parent_variants = List.map parent_chain ~f:(fun p -> "`" ^ (Utils.to_snake_case p |> Utils.sanitize_identifier)) in
-          let all_variants = self_variant :: parent_variants in
-          let variants = String.concat ~sep:" | " all_variants in
-          (class_name, sprintf "[%s] Gobject.obj" variants)
+    ("Record", Option.value record_base_type ~default:"Obj.t")
+  else
+    (* Use hierarchy detection to determine type *)
+    match Hierarchy_detection.get_hierarchy_info ctx class_name with
+    | Some hier_info when hier_info.hierarchy <> MonomorphicType ->
+        (* This class is in a known hierarchy *)
+        let self_variant = "`" ^ (Utils.to_snake_case normalized_class |> Utils.sanitize_identifier) in
+        let parent_variants = build_parent_chain_variants ~normalized_class ~parent_chain ~hier_info in
+        let variants = String.concat ~sep:" | " (self_variant :: parent_variants) in
+        let type_name = match hier_info.hierarchy with
+          | WidgetHierarchy -> "Widget"
+          | EventControllerHierarchy -> "Event controller"
+          | CellRendererHierarchy -> "Cell renderer"
+          | LayoutManagerHierarchy -> "Layout manager"
+          | ExpressionHierarchy -> "Expression"
+          | MonomorphicType -> class_name
+        in
+        (type_name, sprintf "[%s] Gobject.obj" variants)
+    | _ ->
+        (* Monomorphic type or unknown - still use polymorphic variant since all GTK classes inherit from GObject *)
+        let variants = build_hierarchy_variants ~normalized_class ~parent_chain in
+        (class_name, sprintf "[%s] Gobject.obj" variants)
   
 (** Indent content with 2 spaces, preserving empty lines *)
 let print_indent contents buf =
@@ -158,6 +154,56 @@ let convert_out_param_to_ocaml_type ~ctx ~class_name p =
     Some (map_gir_type_to_ocaml ~ctx ~class_name p.param_type ~is_nullable:p.nullable)
   | In | InOut -> None
 
+(** Check if we should generate an accessor method for this class *)
+let should_generate_accessor ~class_name hier_info =
+  (* Only generate accessor if this is NOT the hierarchy root itself *)
+  (* (e.g., Button should have as_widget, but Widget should not have as_widget : t -> Widget.t) *)
+  class_name <> hier_info.gir_root
+
+(** Build the base type reference for a hierarchy accessor *)
+let build_accessor_base_type ~ctx ~hier_info =
+  let gir_root_class = hier_info.gir_root in
+  (* Check if the root class is in a cyclic group *)
+  match Hashtbl.find_opt ctx.module_groups gir_root_class with
+  | Some combined_module_name ->
+      let simple_module_name = Utils.module_name_of_class gir_root_class in
+      (* Check if this is actually a cyclic module (combined != simple) *)
+      if combined_module_name <> simple_module_name then
+        (* For cyclic modules, use CombinedModule.ClassName.t *)
+        combined_module_name ^ "." ^ simple_module_name ^ ".t"
+      else
+        (* Single-class module, just use ClassName.t *)
+        simple_module_name ^ ".t"
+  | None ->
+      (* Not in module_groups table at all, use simple ClassName.t *)
+      Utils.module_name_of_class gir_root_class ^ ".t"
+
+(** Format an accessor declaration based on output mode *)
+let format_accessor_declaration ~output_mode ~accessor ~base_type =
+  match output_mode with
+  | Implementation ->
+      sprintf "let %s (obj : t) : %s = Obj.magic obj\n\n" accessor base_type
+  | Interface ->
+      sprintf "val %s : t -> %s\n\n" accessor base_type
+
+(** Build constructor signature string from parameters *)
+let build_constructor_signature ~ctx ~class_name ctor =
+  let param_types = List.map ~f:(map_constructor_param ~ctx ~class_name) ctor.ctor_parameters in
+  (* Generate signature: param1 -> param2 -> ... -> t *)
+  if List.length param_types = 0 then
+    "unit -> t"
+  else
+    String.concat ~sep:" -> " (param_types @ ["t"])
+
+(** Format constructor external declaration with bytecode/native support *)
+let format_constructor_external ~ocaml_ctor_name ~signature ~ml_name ctor =
+  let param_count = if List.length ctor.ctor_parameters = 0 then 1 else List.length ctor.ctor_parameters in
+  if param_count > 5 then
+    sprintf "external %s : %s = \"%s_bytecode\" \"%s_native\"\n\n"
+      ocaml_ctor_name signature ml_name ml_name
+  else
+    sprintf "external %s : %s = \"%s\"\n\n" ocaml_ctor_name signature ml_name
+
 let generate_ml_interface_internal
     ~ctx
     ~output_mode
@@ -182,40 +228,18 @@ let generate_ml_interface_internal
   | _ -> 
     bprintf buf "type t = %s\n\n" base_type;
 
-  (* Generate accessor method for hierarchy types *)
+(* Generate accessor method for hierarchy types *)
   (match Hierarchy_detection.get_hierarchy_info ctx class_name with
-   | Some hier_info when hier_info.hierarchy <> MonomorphicType ->
-       (* Only generate accessor if this is NOT the hierarchy root itself *)
-       (* (e.g., Button should have as_widget, but Widget should not have as_widget : t -> Widget.t) *)
-       if class_name <> hier_info.gir_root then begin
-         let accessor = hier_info.accessor_method in
-         (* Build the base type reference using the GIR root name *)
-         let gir_root_class = hier_info.gir_root in
-         (* Check if the root class is in a cyclic group *)
-         let base_type =
-           match Hashtbl.find_opt ctx.module_groups gir_root_class with
-           | Some combined_module_name ->
-               let simple_module_name = Utils.module_name_of_class gir_root_class in
-               (* Check if this is actually a cyclic module (combined != simple) *)
-               if combined_module_name <> simple_module_name then
-                 (* For cyclic modules, use CombinedModule.ClassName.t *)
-                 combined_module_name ^ "." ^ simple_module_name ^ ".t"
-               else
-                 (* Single-class module, just use ClassName.t *)
-                 simple_module_name ^ ".t"
-           | None ->
-               (* Not in module_groups table at all, use simple ClassName.t *)
-               Utils.module_name_of_class gir_root_class ^ ".t"
-         in
-         (* Simplify self-references (though this should never happen for accessors) *)
-         let base_type = simplify_self_reference ~class_name base_type in
-         match output_mode with
-         | Implementation ->
-           bprintf buf "let %s (obj : t) : %s = Obj.magic obj\n\n" accessor base_type
-         | Interface ->
-           bprintf buf "val %s : t -> %s\n\n" accessor base_type
-       end
-   | _ -> ());
+    | Some hier_info when hier_info.hierarchy <> MonomorphicType ->
+        if should_generate_accessor ~class_name hier_info then begin
+          let accessor = hier_info.accessor_method in
+          let base_type = build_accessor_base_type ~ctx ~hier_info in
+          (* Simplify self-references (though this should never happen for accessors) *)
+          let base_type = simplify_self_reference ~class_name base_type in
+          let declaration = format_accessor_declaration ~output_mode ~accessor ~base_type in
+          bprintf buf "%s" declaration
+        end
+    | _ -> ());
 
   (* Constructors - generate unique names and proper signatures, skip those that throw or have cross-namespace types *)
   List.iter ~f:(fun (ctor : gir_constructor) ->
@@ -236,24 +260,9 @@ let generate_ml_interface_internal
         Utils.ocaml_constructor_name ~class_name ctor
       in
 
-    (* Build parameter types for constructor signature *)
-    let param_types = List.map ~f:(map_constructor_param ~ctx ~class_name) ctor.ctor_parameters in
-
-    (* Generate signature: param1 -> param2 -> ... -> t *)
-    let signature =
-      if List.length param_types = 0 then
-        "unit -> t"
-      else
-        String.concat ~sep:" -> " (param_types @ ["t"])
-    in
-
-    (* For >5 parameters, reference bytecode/native variants *)
-    let param_count = if List.length ctor.ctor_parameters = 0 then 1 else List.length ctor.ctor_parameters in
-    if param_count > 5 then
-      bprintf buf "external %s : %s = \"%s_bytecode\" \"%s_native\"\n\n"
-        ocaml_ctor_name signature ml_name ml_name
-    else
-      bprintf buf "external %s : %s = \"%s\"\n\n" ocaml_ctor_name signature ml_name;
+      let signature = build_constructor_signature ~ctx ~class_name ctor in
+      let decl = format_constructor_external ~ocaml_ctor_name ~signature ~ml_name ctor in
+      bprintf buf "%s" decl
     end
   ) (constructors |> Option.value ~default:[]);
 
